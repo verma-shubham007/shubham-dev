b688f76fe8226fcd88205ea930730b2daab89026
Shubham <verma_shubham@outlook.com>
2024-06-03 17:52:52 +0530

    contact changes

--- src/Components/Pages/Contact/contact.js
+++ src/Components/Pages/Contact/contact.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [description, setDescription] = useState('');
+    const [submitEnabled, setSubmitEnabled] = useState(true);
+
+    useEffect(() => {
+        setSubmitEnabled(fullName && fullName.length > 0 && email && email.length > 0 && description && description.length > 0 );
+    }, [fullName, email, description]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const emailData = {
+            from_name: fullName,
+            from_email: email,
+            message: description
+        }
+        emailjs.send('service_y2cutre', 'template_d3uqwi8', emailData, 'WhISDjrsrmXTX3o_J')
+            .then((response) => {
+                console.log('Email sent successfully!', response.status, response.text);
+                alert('Email sent successfully!');
+                setFullName('');
+                setEmail('');
+                setDescription('');
+            })
+            .catch((err) => {
+                console.error('Failed to send email. Error: ', err);
+                alert('Failed to send email');
+            });
+    };
+
     return (
         <article className="contact active" data-page="contact">
 
@@ -8,13 +39,13 @@ const Contact = () => {
                 <h2 className="h2 article-title">Contact</h2>
             </header>
 
-            <section className="mapbox">
-                <figure>
-                    <iframe
-                        src="https://www.google.com/"
-                        width="400" height="300" loading="lazy"></iframe>
-                </figure>
-            </section>
+            {/*<section className="mapbox">*/}
+            {/*    <figure>*/}
+            {/*        <iframe*/}
+            {/*            src="https://www.google.com/"*/}
+            {/*            width="400" height="300" loading="lazy"></iframe>*/}
+            {/*    </figure>*/}
+            {/*</section>*/}
 
             <section className="contact-form">
 
@@ -23,14 +54,13 @@ const Contact = () => {
                 <form action="#" className="form">
 
                     <div className="input-wrapper">
-                        <input type="text" name="fullname" className="form-input" placeholder="Full name" required />
-
-                            <input type="email" name="email" className="form-input" placeholder="Email address" required />
+                        <input type="text" name="fullname" value={fullName} onChange={(e) => setFullName(e.target.value)} className="form-input" placeholder="Full name" required />
+                        <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-input" placeholder="Email address" required />
                     </div>
 
-                    <textarea name="message" className="form-input" placeholder="Your Message" required></textarea>
+                    <textarea name="message" className="form-input" placeholder="Your Message" required value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
 
-                    <button className="form-btn" type="submit" disabled>
+                    <button className="form-btn" type="submit" disabled={!submitEnabled} onClick={(e) => handleSubmit(e)}>
                         <ion-icon name="paper-plane"></ion-icon>
                         <span>Send Message</span>
                     </button>
